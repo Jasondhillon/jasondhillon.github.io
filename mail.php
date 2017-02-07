9c2be6a85575f935e3b498e4ae4299bf06ca15a0
Jason <padbangboom@yahoo.com>
2017-02-07 15:33:24 -0500

    Added Contact form people

--- mail.php
+++ mail.php
@@ -4,9 +4,10 @@
 		$email = $_POST['email'];
 		$phone = $_POST['phone'];
 		$subject = $_POST['subject'];
+		$people = $_POST['people'];
 		$message = $_POST['message'];
 
-		$body = $message . "\n" . "From " . $name . "\n" . "My email is " . $email . "\n" . $phone;
+		$body = "Hi my name is " . $name .  "\n" . "We have " . $people . "coming with us.\n\n" . $message . "\n" . "My email is " . $email . "\n" . $phone;
 		$header = "From: $email";
 
 		if($_POST['submit']){
